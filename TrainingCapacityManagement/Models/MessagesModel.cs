42721abdd402a1dd276898ba42ba155667711e51
Bernd Mayer <bmayer@microsoft.com>
2020-09-06 16:48:50 +0200

    Germanification and Old Service Dependencies removed

---
+++ TrainingCapacityManagement/Models/MessagesModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingCapacityManagement.Models
+{
+    public class MessagesModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Überschrift")]
+        public string Title { get; set; }
+        [Display(Name = "Text")]
+        public string Body { get; set; }
+    }
+}
